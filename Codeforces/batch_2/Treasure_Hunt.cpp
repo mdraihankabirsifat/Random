1030b2942abbca3181e37665e449a9cf0bbc0ba6
Sifat <www.raihankabireusc@gmail.com>
2025-08-04 23:58:56 +0600

    updated

---
+++ Codeforces/batch_2/Treasure_Hunt.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, a;
+        cin >> x >> y >> a;
+        
+    }
+    return 0;
+}
