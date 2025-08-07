c3bb11952df59a1ff7e9687f98744b77d2f305c4
Sifat <www.raihankabireusc@gmail.com>
2025-08-08 00:56:38 +0600

    o

--- Codeforces/batch_1/Generate_login.cpp
+++ Codeforces/batch_1/Generate_login.cpp
@@ -1,35 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    char c;
-    int p, t;
-    vector<char> s;
-    while (cin.get(c) && c != '\n')
+    string a, b, result;
+    cin >> a >> b;
+    result.push_back(a[0]); // start with first letter of a
+    for (int i = 1; i < (int)a.size(); i++)
     {
-        s.push_back(c);
-    }
-    for (int i = 0; i < s.size() - 1; i++)
-    {
-        if (s[i] == ' ')
-        {
-            p = s[i + 1];
-            t = i;
-            break;
-        }
-    }
-    cout << s[0];
-    for (int i = 1; i < t; i++)
-    {
-        if (s[i] < p)
-        {
-            cout << s[i];
-        }
+        if (a[i] < b[0])
+            result.push_back(a[i]); // add letters smaller than b[0]
         else
-        {
             break;
-        }
     }
-    cout << s[t + 1];
+    result.push_back(b[0]);
+    cout << result << "\n";
     return 0;
 }
