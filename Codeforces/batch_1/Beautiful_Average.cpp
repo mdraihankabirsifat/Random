216f2825c0205ff2c0663b7e5433082014e69199
Sifat <www.raihankabireusc@gmail.com>
2025-10-23 01:02:23 +0600

    gigi

---
+++ Codeforces/batch_1/Beautiful_Average.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        cout << *max_element(v.begin(), v.end()) << "\n";
+    }
+    return 0;
+}
