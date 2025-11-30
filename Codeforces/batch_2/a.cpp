9e489ef845f0c10aca37494c00139a15683d2b79
Sifat <www.raihankabireusc@gmail.com>
2025-12-01 00:28:32 +0600

    gigi

--- Codeforces/batch_2/a.cpp
+++ Codeforces/batch_2/a.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
-#define msort(x) sort(x.begin(), x.end())
-#define endl '\n'
-#define loop(i, n) for (int i = 0; i < n; i++)
+#define jora(p, n) pair<ll, ll> p(n)
+#define sajai(x) sort(x.begin(), x.end())
+#define arr(v, x, y) vector<vector<ll>> v(x, vector<int>(y))
+#define tata '\n'
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define in(v) loop(i, v.size()) cin >> v[i]
+#define out(v) loop(i, v.size()) cout << v[i] << " "
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
+    ll t;
     cin >> t;
     while (t--)
     {
         ll n;
         cin >> n;
         vector<int> v(n);
-        loop(i, n)
-        {
-            cin >> v[i];
-        }
+        in(v);
         
     }
     return 0;
