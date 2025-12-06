d46eb9abb0b8e0458baa650fc4155841fdd3d1b5
Sifat <www.raihankabireusc@gmail.com>
2025-12-07 02:01:08 +0600

    Trionda

--- CSE/Online & Offline/3_Array_C23.c
+++ CSE/Online & Offline/3_Array_C23.c
@@ -7,13 +7,31 @@
 #define out(arr) loop(i, 0, n) printf("%lld ", arr[i])
 #define no printf("NO\n")
 #define yes printf("YES\n")
+#define N 1000000
+ll p[N];
 
 int main()
 {
-    ll n;
+    ll n, l = 1;
     scanf("%lld", &n);
     ll arr[n];
     in(arr);
-    
+    for (int i = 0; i < n; i++)
+    {
+        p[arr[i]]++;
+    }
+    for (int i = 1; i < N; i++)
+    {
+
+        if (p[i] > n / 2)
+        {
+            l = 0;
+            printf("%lld ", i);
+        }
+    }
+    if (l)
+    {
+        printf("No Majority Candidate\n");
+    }
     return 0;
 }
