5b3fbeb4dcc82b696b09e86492858cce0cd01311
Sifat <www.raihankabireusc@gmail.com>
2026-01-17 23:43:29 +0600

    updated

--- Personal_Funtion/Disgusting_UI/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WorstCaptcha from './p3'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WorstCaptcha />
-  </React.StrictMode>
-)
