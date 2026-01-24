e5bf2500a9fc484f9550914baa85a867bf8fec8a
Sifat <www.raihankabireusc@gmail.com>
2026-01-24 21:44:53 +0600

    offline_2

--- CSE/Online & Offline/106/Offline_2/syntax_checker.cpp
+++ CSE/Online & Offline/106/Offline_2/syntax_checker.cpp
@@ -1,5 +1,8 @@
-#include <stack>
-#include <string>
+#include <bits/stdc++.h>
+#define tata "\n"
+#define ll long long
+#define pb push_back
+#define loop(i, j, n) for (ll i = j; i < n; i++)
 using namespace std;
 
 /**
@@ -10,6 +13,12 @@ using namespace std;
  */
 bool isMatchingPair(char opening, char closing)
 {
+    if (opening == '(' && closing == ')')
+        return true;
+    if (opening == '[' && closing == ']')
+        return true;
+    if (opening == '{' && closing == '}')
+        return true;
     // TODO: Implement this helper function
     // Check if opening and closing brackets match
     // Valid pairs: (), [], {}
@@ -24,6 +33,26 @@ bool isMatchingPair(char opening, char closing)
 bool isValidExpression(const string &expression)
 {
     // TODO: Implement the syntax checker using std::stack
-
-    return false; // Replace this with your implementation
+    stack<char> s;
+    for (char c : expression)
+    {
+        if (c == '(' || c == '[' || c == '{')
+        {
+            s.push(c);
+        }
+        else if (c == ')' || c == ']' || c == '}')
+        {
+            if (s.empty())
+            {
+                return false;
+            }
+            char t = s.top();
+            s.pop();
+            if (!isMatchingPair(t, c))
+            {
+                return false;
+            }
+        }
+    }
+    return s.empty(); // Replace this with your implementation
 }
