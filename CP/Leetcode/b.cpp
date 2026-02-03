d77d5b4a62e0253e6a719de28a66be6549856e9e
Sifat <www.raihankabireusc@gmail.com>
2026-02-04 01:41:26 +0600

    Leetcode solving..

--- CP/Leetcode/b.cpp
+++ CP/Leetcode/b.cpp
@@ -1,2 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+class Solution
+{
+public:
+    bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
+    {
+        
+    }
+};
