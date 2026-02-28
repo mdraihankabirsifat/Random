03f828c9a4e890ffbd0ecd85d906bcc8ea1984a0
Sifat <www.raihankabireusc@gmail.com>
2026-02-28 19:32:46 +0600

    offline 6

--- CP/batch_2/a.cpp
+++ CP/batch_2/a.cpp
@@ -1,38 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define ll long long
-#define pb push_back
-#define tata "\n"
-#define vr(v, x) vector<ll> v(x)
-#define vrr(v, x, y) vector<vector<ll>> v(x, vector<ll>(y))
-#define jora(p, n) pair<ll, ll> p(n)
-#define sajai(x) sort(x.begin(), x.end())
-#define rsort(a) sort(a.begin(), a.end(), greater<>())
-#define ulta(a) reverse(a.begin(), a.end())
-#define mucho(a, j) a.erase(a.begin() + j)
-#define choto(a) *min_element(a.begin(), a.end())
-#define boro(a) *max_element(a.begin(), a.end())
-#define jog(a) accumulate(a.begin(), a.end(), 0LL)
-#define bit(n) __builtin_popcountll(n)
-#define loop(i, j, n) for (ll i = j; i < n; i++)
-#define in(v) loop(i, 0, v.size()) cin >> v[i]
-#define out(v) loop(i, 0, v.size()) cout << v[i] << " "
-#define yes cout << "YES" << tata
-#define no cout << "NO" << tata
+
+int countFreq(vector<int> &arr, int candidate, int low, int high)
+{
+    int count = 0;
+    for (int i = low; i <= high; i++)
+    {
+        if (arr[i] == candidate)
+            count++;
+    }
+    return count;
+}
+
+int majorityElement(vector<int> &arr, int low, int high)
+{
+    // Base case
+    if (low == high)
+        return arr[low];
+
+    int mid = low + (high - low) / 2;
+
+    // Divide
+    int leftMajor = majorityElement(arr, low, mid);
+    int rightMajor = majorityElement(arr, mid + 1, high);
+
+    // Conquer
+    if (leftMajor == rightMajor)
+        return leftMajor;
+
+    int leftCount = countFreq(arr, leftMajor, low, high);
+    int rightCount = countFreq(arr, rightMajor, low, high);
+
+    return (leftCount > rightCount) ? leftMajor : rightMajor;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n;
-        cin >> n;
-        vr(v, n);
-        in(v);
-        
-    }
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+    int n = arr.size();
+    cout << "Majority Element: " << majorityElement(arr, 0, n - 1);
     return 0;
 }
