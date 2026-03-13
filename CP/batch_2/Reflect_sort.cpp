f459c511fab6fcc5783d86f364897ae1c17b8f67
Sifat <www.raihankabireusc@gmail.com>
2026-03-13 15:39:19 +0600

    Reflect sort

---
+++ CP/batch_2/Reflect_sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define tata "\n"
+#define vr(v, x) vector<ll> v(x)
+#define vrr(v, x, y) vector<vector<ll>> v(x, vector<ll>(y))
+#define jora(p, n) pair<ll, ll> p(n)
+#define sajai(x) sort(x.begin(), x.end())
+#define rsort(a) sort(a.begin(), a.end(), greater<>())
+#define ulta(a) reverse(a.begin(), a.end())
+#define mucho(a, j) a.erase(a.begin() + j)
+#define choto(a) *min_element(a.begin(), a.end())
+#define boro(a) *max_element(a.begin(), a.end())
+#define jog(a) accumulate(a.begin(), a.end(), 0LL)
+#define bit(n) __builtin_popcountll(n)
+#define loop(i, j, n) for (ll i = j; i < n; i++)
+#define in(v) loop(i, 0, v.size()) cin >> v[i]
+#define out(v) loop(i, 0, v.size()) cout << v[i] << " "
+#define yes cout << "YES" << tata
+#define no cout << "NO" << tata
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n, g = 0;
+    cin >> n;
+    vector<ll> v(n);
+    in(v);
+    loop(i, 1, n)
+    {
+        g = __gcd(g, 2 * abs(v[i] - v[i - 1]));
+    }
+    if (g)
+    {
+        g = (v[0] - 1) % g + 1;
+    }
+    else
+    {
+        g = v[0];
+    }
+    loop(i, 1, n)
+    {
+        g += abs(v[i] - v[i - 1]);
+    }
+    cout << g << tata;
+    return 0;
+}
