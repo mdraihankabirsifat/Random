655cd29fc7a7840be263d59bb28ec1cc0257eeb7
Sifat <www.raihankabireusc@gmail.com>
2026-03-31 03:02:33 +0600

    added details

--- CSE/OOP/ADR/ADR Sir Codes/Java-Networking/tcpsimple/Server.java
+++ CSE/OOP/ADR/ADR Sir Codes/Java-Networking/tcpsimple/Server.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -23,13 +24,20 @@ public class Server {
 
     private void serve(Socket clientSocket) throws IOException, ClassNotFoundException {
         clientCount++;
-        try (ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-             ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream())) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream()); ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream())) {
             System.out.println(ois.readObject() + "," + clientCount);
             oos.writeObject("Hello Client : " + clientCount);
         }
     }
+
     public static void main(String args[]) {
         new Server();
     }
 }
+
+// Packages:
+// - tcpsimple (no threading)
+// - tcpstring (multithreading, string send and receive)
+// - tcpobject (multithreading, object send and receive)
+// - tcpdiff (multithreading, the server sends messages to multiple clients)
+// - tcpforward (multithreading, the server forwards messages between multiple clients)
