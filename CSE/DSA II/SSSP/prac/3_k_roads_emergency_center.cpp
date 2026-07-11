1f562173bea55196f1b81c38bcfc8932adc6d768
Sifat <www.raihankabireusc@gmail.com>
2026-07-12 02:31:55 +0600

    updated

--- CSE/DSA II/SSSP/prac/3_k_roads_emergency_center.cpp
+++ CSE/DSA II/SSSP/prac/3_k_roads_emergency_center.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 #define pb push_back
 #define tata "\n"
-
 const ll INF = 4e18;
 
 struct Edge
@@ -13,24 +11,18 @@ struct Edge
 };
 
 // 2. Reach an emergency center using at most K roads
-vector<ll> bellmanFord(
-    ll n,
-    vector<Edge> &edges,
-    vector<ll> &centers,
-    ll maxRoads)
+vector<ll> bellmanFord(ll n, vector<Edge> &edges, vector<ll> &centers, ll maxRoads)
 {
     vector<ll> dist(n + 1, INF);
-
     for (ll center : centers)
+    {
         dist[center] = 0;
-
+    }
     maxRoads = min(maxRoads, n - 1);
-
     for (ll i = 1; i <= maxRoads; i++)
     {
         vector<ll> nextDist = dist;
         bool changed = false;
-
         for (Edge edge : edges)
         {
             if (dist[edge.u] != INF &&
@@ -40,13 +32,10 @@ vector<ll> bellmanFord(
                 changed = true;
             }
         }
-
         dist = nextDist;
-
         if (!changed)
             break;
     }
-
     return dist;
 }
 
@@ -54,29 +43,22 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     ll n, m, centerCount, maxRoads;
     cin >> n >> m >> centerCount >> maxRoads;
-
     vector<ll> centers(centerCount);
-
     for (ll &center : centers)
+    {
         cin >> center;
-
+    }
     vector<Edge> edges;
-
     for (ll i = 0; i < m; i++)
     {
         ll u, v, w;
         cin >> u >> v >> w;
-
         // Reverse edge
         edges.pb({v, u, w});
     }
-
-    vector<ll> dist =
-        bellmanFord(n, edges, centers, maxRoads);
-
+    vector<ll> dist = bellmanFord(n, edges, centers, maxRoads);
     for (ll node = 1; node <= n; node++)
     {
         if (dist[node] == INF)
@@ -84,6 +66,5 @@ int main()
         else
             cout << dist[node] << tata;
     }
-
     return 0;
 }
