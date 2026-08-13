cd84f36eb40101cf9f923f797010462c4f9dbda5
Sifat <www.raihankabireusc@gmail.com>
2026-08-13 14:19:44 +0600

    solving daily leet problems

---
+++ CP/Leetcode/Cur/25_longest_substring.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define tata "\n"
+#define vr(v, x) vector<ll> v(x)
+#define vrr(v, x, y) vector<vector<ll>> v(x, vector<ll>(y))
+#define jora(p, n) pair<ll, ll> p(n)
+#define sajai(x) sort(x.begin(), x.end())
+#define rsort(a) sort(a.begin(), a.end(), greater<>())
+#define ulta(a) reverse(a.begin(), a.end())
+#define mucho(a, j) a.erase(a.begin() + j)
+#define choto(a) *min_element(a.begin(), a.end())
+#define boro(a) *max_element(a.begin(), a.end())
+#define jog(a) accumulate(a.begin(), a.end(), 0LL)
+#define bit(n) __builtin_popcountll(n)
+#define loop(i, j, n) for (ll i = j; i < n; i++)
+#define in(v) loop(i, 0, v.size()) cin >> v[i]
+#define out(v) loop(i, 0, v.size()) cout << v[i] << " "
+#define yes cout << "YES" << tata
+#define no cout << "NO" << tata
+
+class Solution
+{
+public:
+    vector<int> longestRepeating(string s, string queryCharacters, vector<int> &queryIndices)
+    {
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s, s1;
+    vector<int> v = {};
+    Solution sol;
+    v = sol.longestRepeating(s, s1, v);
+    out(v);
+    return 0;
+}
