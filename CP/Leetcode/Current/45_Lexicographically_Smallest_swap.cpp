0ebdbad048647d95282239025d099f2f3d84ab28
Sifat <www.raihankabireusc@gmail.com>
2026-08-29 22:11:10 +0600

    solved LC daily

--- CP/Leetcode/Current/45_Lexicographically_Smallest_swap.cpp
+++ CP/Leetcode/Current/45_Lexicographically_Smallest_swap.cpp
@@ -1,4 +1,4 @@
-// 3734
+// 2948
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -7,27 +7,42 @@ using namespace std;
 #define loop(i, j, n) for (ll i = j; i < n; i++)
 #define in(v) loop(i, 0, v.size()) cin >> v[i]
 #define out(v) loop(i, 0, v.size()) cout << v[i] << " "
+#define sajai(x) sort(x.begin(), x.end())
 
 class Solution
 {
 public:
     vector<int> lexicographicallySmallestArray(vector<int> &v, int k)
     {
-        // 1,3,5,8,9
         ll n = v.size();
         if (n == 1)
         {
             return v;
         }
-        for (int i = n - 1; i > 0; i--)
+        vector<vector<int>> a;
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < i; j++)
+            a.pb({v[i], i});
+        }
+        sajai(a);
+        for (int i = 0; i < n;)
+        {
+            int j = i;
+            while (j + 1 < n && a[j + 1][0] - a[j][0] <= k)
             {
-                if (abs(v[j] - v[i] <= k) && v[i] < v[j])
-                {
-                    swap(v[j], v[i]);
-                }
+                j++;
             }
+            vector<int> v1;
+            for (int x = i; x <= j; x++)
+            {
+                v1.pb(a[x][1]);
+            }
+            sajai(v1);
+            for (int x = i; x <= j; x++)
+            {
+                v[v1[x - i]] = a[x][0];
+            }
+            i = j + 1;
         }
         return v;
     }
@@ -37,9 +52,9 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> v = {1};
+    vector<int> v = {4, 52, 38, 59, 71, 27, 31, 83, 88, 10};
     string s1 = "";
-    ll n = 1;
+    ll n = 14;
     Solution sol;
     v = sol.lexicographicallySmallestArray(v, n);
     out(v);
