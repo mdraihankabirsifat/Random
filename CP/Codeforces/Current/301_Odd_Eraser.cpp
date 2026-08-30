59994adbd6a9f37cb53a28a590e885f5410b43dc
Sifat <www.raihankabireusc@gmail.com>
2026-08-30 23:06:30 +0600

    solved CF prob

---
+++ CP/Codeforces/Current/301_Odd_Eraser.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define tata "\n"
+#define vrr(v, x, y) vector<vector<ll>> v(x, vector<ll>(y))
+#define sajai(x) sort(x.begin(), x.end())
+#define choto(a) *min_element(a.begin(), a.end())
+#define boro(a) *max_element(a.begin(), a.end())
+#define jog(a) accumulate(a.begin(), a.end(), 0LL)
+#define bit(n) __builtin_popcountll(n)
+#define loop(i, j, n) for (ll i = j; i < n; i++)
+#define in(v) loop(i, 0, v.size()) cin >> v[i]
+#define out(v) loop(i, 0, v.size()) cout << v[i] << " "
+#define yes cout << "YES" << tata
+#define no cout << "NO" << tata
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        in(v);
+        cout << gcd(v[0],v[n-1])<< tata;
+    }
+    return 0;
+}
