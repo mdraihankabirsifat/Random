a94d3122afdae033b05f7efcc767f327d59bd9b1
Sifat <www.raihankabireusc@gmail.com>
2026-09-02 21:38:26 +0600

    solved LC daily

---
+++ CP/Leetcode/Current/49_construct_parity.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define tata "\n"
+#define vrr(v, x, y) vector<vector<ll>> v(x, vector<ll>(y))
+#define sajai(x) sort(x.begin(), x.end())
+#define loop(i, j, n) for (ll i = j; i < n; i++)
+#define in(v) loop(i, 0, v.size()) cin >> v[i]
+#define out(v) loop(i, 0, v.size()) cout << v[i] << " "
+#define yes cout << "YES" << tata
+#define no cout << "NO" << tata
+
+class Solution
+{
+public:
+    bool uniformArray(vector<int> &nums1)
+    {
+        return true;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<int> v = {};
+    string s = "";
+    ll n;
+    Solution sol;
+    // cout << sol.;
+    return 0;
+}
